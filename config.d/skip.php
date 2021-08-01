48f17c939888b838d0796176878264ccee759535
Roman v. Gemmeren <roman.van-gemmeren@chefkoch.de>
2021-08-01 13:10:00 +0200

    + update config

---
+++ config.d/skip.php
@@ -0,0 +1,4 @@
+<?php
+# Set SELF_URL_PATH based on request                                                                                               
+define('_SKIP_SELF_URL_PATH_CHECKS', true);
+?>
